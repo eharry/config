60aee643dc72a8077bb397cd5aab39955fa567ac
eharry <eharry@foxmail.com>
2018-05-15 21:03:57 +0800

    update

--- bin/buildTags.sh
+++ bin/buildTags.sh
@@ -6,7 +6,7 @@ rm -rf cscope.in.out cscope.out cscope.po.out filenametags files tags
 echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > filenametags
 gfind . -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | sort -f >> filenametags
 
-gfind . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.java" > files
+gfind . -name "*.h" -o -name "*.i" -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" -o -name "*.java" > files
 
 
 ctags  -R --c++-kinds=+p --fields=+iaS --extras=+q --language-force=C++ -L -< files
